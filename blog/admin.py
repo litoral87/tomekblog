7da04afd1b4eb2cbda12b9debd106dd5885b2d25
litoral87 <115455869+litoral87@users.noreply.github.com>
2022-11-01 22:45:52 +0100

    komentarz o klasie

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 #importujemy modele z klasy POST - którą sobie utworzyliśmy
 from .models import Post
 
-#rejestrujemy model z klasy post
+#rejestrujemy model z klasy post - uwaga te zmiany
 admin.site.register(Post)
 
